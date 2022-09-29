0a61113904cc652b4694ecfd5ad2c514c0d727c6
Shubham Yadav <shubhamdv13@gmail.com>
2022-09-29 17:14:59 +0530

    correction_done

--- Prime2.js
+++ Prime2.js
@@ -1,13 +1,13 @@
 function checkPrime(num){
 
 let count=0;
-for(let i=0; i<=num; i++){
+for(let i=1; i<=num; i++){
   if(num%i===0){
   count++;
 
 }
 }
- if(count===1){
+ if(count===2){
    return true;
 }
 else{
